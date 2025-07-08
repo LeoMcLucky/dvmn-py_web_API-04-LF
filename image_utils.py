8d81818e6d13a4d3f9820b5ef82be12362932def
Lev <leon.fissenko@gmail.com>
2025-07-08 16:37:10 +0500

    Made the code flatter

--- image_utils.py
+++ image_utils.py
@@ -15,9 +15,10 @@ def get_paths_images(path_dir):
     paths_all_images = []
     for dirpath, _, filenames in os.walk(path_dir):
         for filename in filenames:
-            if not filename.startswith('.'):
-                file_path = Path(dirpath) / filename
-                paths_all_images.append(file_path)
+            if filename.startswith('.'):
+                continue
+            file_path = Path(dirpath) / filename
+            paths_all_images.append(file_path)
     return paths_all_images
 
 
